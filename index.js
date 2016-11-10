e13853a11bc3c1a45a52c1f0f7bdd6925acabfbf
Philhawk <student@Dans-MacBook-Pro-2.local>
2016-11-10 11:01:36 -0500

    sendMessage ability

--- index.js
+++ index.js
@@ -31,3 +31,21 @@ app.post('/webhook', function (req, res) {
     }
     res.sendStatus(200);
 });
+
+function sendMessage(recipientId, message) {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};
